c905a7a03a3315834e918a2840eb2817d7fe7c3f
Shadowfax224 <sahilshahpatel@gmail.com>
2019-02-16 17:35:12 -0600

    Including parser.py
    No implementation, only descriptions

---
+++ parser.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import re
+import os
+
+#Fill in array with the chord names/durations
+#return (tonic, chordlist)
+def getChordList(filepath):
+    pass
+
+# Call getChorList for each file inside parent folder and organize based on tonic
+# return dictionary (key, value) = (tonic, 2d array where each row is the chord list of a song)
+def getAllData(location_of_parent_folder):
+    pass
+    
