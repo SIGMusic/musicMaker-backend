22f38d535665ad2433c14dfad4dff6932a07262b
Shadowfax224 <sahilshahpatel@gmail.com>
2019-03-01 15:57:27 -0600

    Parser now gives duration in units of measures rather than in occurances
    Also refactored all the "beat" variables into "measure" variables so that they accurately reflect what they mean in context.

--- parser.py
+++ parser.py
@@ -41,37 +41,46 @@ def get_chord_list (filepath):
             while remaining_line.find("|") != -1:
                 next_bar = remaining_line.find("|")
                 # Beat is in between bars ("|"), can contain multiple chords
-                beat = remaining_line[:next_bar]
+                measure = remaining_line[:next_bar]
 
-                # Chop up beats by chord, identified by spaces
-                beat_chord_list = []
-                remaining_beat = beat[beat.find(" ") + 1:]
-                while remaining_beat.find(" ") != -1:
-                    #print("Remaining Beat: " + remaining_beat)
-                    next_space = remaining_beat.find(" ")
+                # Chop up measure by chord, identified by spaces
+                measure_chord_list = []
+                remaining_measure = measure[measure.find(" ") + 1:]
+                while remaining_measure.find(" ") != -1:
+                    #print("Remaining Measure: " + remaining_beat)
+                    next_space = remaining_measure.find(" ")
                     # Chord is in between spaces, if it contains a colon
-                    chord_name = remaining_beat[:next_space]
+                    chord_name = remaining_measure[:next_space]
                     repeat = False
                     if(chord_name.find(".")==-1):
                         if(chord_name.find(":")==-1):
-                            remaining_beat = remaining_beat[next_space + 1:]
+                            remaining_measure = remaining_measure[next_space + 1:]
                             continue
                     else:
                         repeat = True
                     #print("Chord: " + chord_name)
-                    if len(beat_chord_list) != 0 and repeat:
-                        beat_chord_list[-1].duration += 1
+                    if len(measure_chord_list) != 0 and repeat:
+                        measure_chord_list[-1].duration += 1
                     else:
-                        beat_chord_list.append(Chord(chord_name, 1))
-                    remaining_beat = remaining_beat[next_space + 1:]
+                        measure_chord_list.append(Chord(chord_name, 1))
+                    remaining_measure = remaining_measure[next_space + 1:]
 
-                # Check for repeats between beats
-                if len(section_chord_list) != 0 and section_chord_list[-1].chord == beat_chord_list[0].chord:
-                    section_chord_list[-1].duration += 1
-                    beat_chord_list.pop(0)
+                # Convert duration measurements into units of measures
+                #  First, find total length of chords in measure_chord_list
+                chord_duration_sum = 0
+                for chord in measure_chord_list:
+                    chord_duration_sum += chord.duration
+                # Next divide by that sum
+                for chord in measure_chord_list:
+                    chord.duration/=chord_duration_sum
 
-                # Concatenate the beat's chords to section_chord_list
-                section_chord_list += beat_chord_list
+                # Check for repeats between measures
+                if len(section_chord_list) != 0 and section_chord_list[-1].chord == measure_chord_list[0].chord:
+                    section_chord_list[-1].duration += measure_chord_list[0].duration
+                    measure_chord_list.pop(0)
+
+                # Concatenate the measure's chords to section_chord_list
+                section_chord_list += measure_chord_list
                 remaining_line = remaining_line[next_bar + 1:]
 
             # account for "xN" multipliers
