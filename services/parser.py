5740d742d36f579c0f889faa30f7b9eed21411d4
Shadowfax224 <sahilshahpatel@gmail.com>
2019-03-07 16:53:18 -0600

    Completed key irrespective features
    I tested them for a general case of tonic = C and every possible root note. However, I haven't tested against an actual song. See comments above key_irrespective_chord() for a description of the returned Chord object

--- services/parser.py
+++ services/parser.py
@@ -2,7 +2,7 @@ import os
 
 # Create Chord class
 class Chord:
-    def __init__(self, chord, duration):
+    def __init__(self, chord, duration = 1):
         self.chord = chord
         self.duration = duration
     def __repr__(self):
@@ -120,9 +120,81 @@ def get_all_data (parent_folder):
             data[tonic[0]] = [chord_list]
     return data
 
+# Convert a Chord object into key irrespective notation for a given tonic
+#  Exs: 1:maj, 2:min, 3:min, 4:maj, 5:maj, 6:min, 7:dim
+#  For minor chords, you may see 3:maj for a III or 7b:maj for a VII in natural minor
+#
+#  Note: chord types (':maj' or ':min') are not converted AT ALL. This function only converts the root name,
+#   hence the use of '7b:maj' for VII, which uses a different 7th than a vii0 would.
+#   Also, "x:__" represents a non-diatonic chord or other error
+def key_irrespective_chord (tonic, chord):
+    # Create notes list to reference later. Only sharps are used. Any flats can look to one index before their base.
+    notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+
+    # Record intervals for a generic scale (2 = whole step, 1 = half step)
+    intervals = [2, 2, 1, 2, 2, 2, 1]
+
+    # Find tonic within the notes list
+    try:
+        tonic_index = notes.index(tonic)
+    except:
+        # If tonic has a flat in it, use the index before the base note
+        tonic_index = notes.index(tonic[:1]) - 1
+
+    error = False
+    chord_root = chord.chord[:chord.chord.find(":")]
+    chord_type = chord.chord[chord.chord.find(":"):]
+    
+    try:
+        chord_index = notes.index(chord_root)
+    except:
+        try:
+            # If chord name has a flat in it, use the index before the base note
+            chord_index = notes.index(chord_root[:1]) - 1
+        except:
+            # Chord name not properly formatted
+            error = True
+    
+    if not error:
+        # Find now much forward the chord_index is compared to the tonic
+        index_difference = (chord_index - tonic_index + len(notes)) % len(notes)
+        interval_sum = 0
+        interval_index_counter = 0
+        diatonic = True
+        minor = False
+        #print("Difference between", tonic, "and", chord_root, ":", index_difference)
+        while diatonic and interval_sum != index_difference:
+            #print("Counter:", interval_index_counter)
+            # If the chord can't be found within our intervals, it is non-diatonic
+            try:
+                # Check for lowered 3rds 6ths and 7ths to check for minor mode
+                if interval_index_counter in [1, 4, 5] and interval_sum + intervals[interval_index_counter] - 1 == index_difference:
+                    minor = True
+                    break
+                interval_sum += intervals[interval_index_counter]
+            except:
+                diatonic = False
+            interval_index_counter += 1
+
+        if minor:
+            # Since a 1 chord is tonic, add 1 to the interval_index_counter
+            #  And, to record the chord as a flat instead of a sharp, add an extra 1
+            #  Note: I chose to have all chords default to a flattened 3, 6, or 7 because I want to keep chord names unique!
+            chord_root_num = str(interval_index_counter + 2) + "b"
+        elif diatonic:
+            # Since a 1 chord is tonic, add 1 to the interval_index_counter
+            chord_root_num = interval_index_counter + 1 
+        else:
+            chord_root_num = "x"
+            #print(chord_root, chord_type, "in key", tonic, "labeled as x")
+
+        return Chord(str(chord_root_num) + chord_type, chord.duration)
+    else:
+        # If something goes wrong, don't add the chord to the new list.
+        #  Seems like I've gotten rid of all errors, but I'm keeping this in here just in case.
+        print("There was an error with chord:", chord)
+
 # Convert a given (tonic, chordList) into key-irrespective form
-#  Ex: [1:maj, 2:min]
-#  "x:__" represents a non-diatonic chord or other error
 #  returns a new chord_list if applicable
 #  return empty list if there are key changes or any errors in tonic
 def key_irrespective_list (tonic_list, chord_list):
@@ -132,75 +204,18 @@ def key_irrespective_list (tonic_list, chord_list):
         tonic = tonic_list[0]
         # If tonic has double sharps or flats, ignore the list
         if len(tonic) <=2:
-            # Create notes list to reference later. Only sharps are used. Any flats can look to one index previous!
-            notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
-
-            # Record intervals for a general scale (2 = whole step, 1 = half step)
-            #  We don't need to know if the song is major or minor because we will just set the tonic to 6
-            #  in the case of a minor, and all the chord_types are preserved
-            intervals = [2, 2, 1, 2, 2, 2, 1]
-
-            # TODO: We don't deal with harmonic or melodic minor AT ALL. This only works for natural minor scales :(
-
-            try:
-                tonic_index = notes.index(tonic)
-            except:
-                # If tonic has a flat in it, use the index before the base note
-                tonic_index = notes.index(tonic[:1]) - 1
-
-            new_chord_list = []
             for section_chord_list in chord_list:
                 new_section_chord_list = []
                 for chord in section_chord_list:
-                    error = False
-                    chord_root = chord.chord[:chord.chord.find(":")]
-                    chord_type = chord.chord[chord.chord.find(":"):]
-                    try:
-                        chord_index = notes.index(chord_root)
-                    except:
-                        try:
-                            # If chord name has a flat in it, use the index before the base note
-                            chord_index = notes.index(chord_root[:1]) - 1
-                        except:
-                            # Chord name not properly formatted
-                            error = True
-                    
-                    if not error:
-                        # Find now much forward the chord_index is compared to the tonic
-                        index_difference = (chord_index - tonic_index + len(notes)) % len(notes)
-                        interval_sum = 0
-                        interval_index_counter = 0
-                        diatonic = True
-                        #print("Difference between", tonic, "and", chord_root, ":", index_difference)
-                        while diatonic and interval_sum != index_difference:
-                            #print("Counter:", interval_index_counter)
-                            # If the chord can't be found within our intervals, it is non-diatonic
-                            try:
-                                interval_sum += intervals[interval_index_counter]
-                            except:
-                                diatonic = False
-                            interval_index_counter += 1
-
-                        if(diatonic):
-                            # Since a 1 chord is tonic, add 1 to the interval_index_counter
-                            chord_root_num = interval_index_counter + 1
-                        else:
-                            chord_root_num = "x"
-
-                        new_section_chord_list.append(Chord(str(chord_root_num) + chord_type, chord.duration))
-                    else:
-                        # If something goes wrong, don't add the chord to the new list.
-                        #  Seems like I've gotten rid of all errors, but I'm keeping this in here just in case.
-                        print("There was an error with chord:", chord)
-
+                    new_section_chord_list.append(key_irrespective_chord(tonic, chord))
                 new_chord_list.append(new_section_chord_list)
                 new_section_chord_list = []
             
     return new_chord_list
-            
-# Call get_chord_list for each file inside parent folder and convert it to key_irrespective
+
+# Call get_chord_list for each file inside parent folder and convert it to key irrespective
 # parent_folder should be set to "../data/McGill-Billboard"
-# return dictionary (key, value) = (tonic, a list of arrays where each array is the chord list of a song)
+# return list of lists where each inner list is the key irrespective chord list of a song
 def get_all_data_key_irrespective (parent_folder):
     # Create list to store chordLists
     data = []
@@ -211,8 +226,15 @@ def get_all_data_key_irrespective (parent_folder):
         data.append(key_irrespective_list(tonic, chord_list))
     return data
 
-# Test
+## Tests ##
+
 #print(get_chord_list("../data/McGill-Billboard/0089/salami_chords.txt"))
-print(get_all_data_key_irrespective("../data/McGill-Billboard"))
+#print(get_all_data_key_irrespective("../data/McGill-Billboard"))
+#get_all_data_key_irrespective("../data/McGill-Billboard")
 #print(get_all_data("../data/McGill-Billboard"))
 #get_all_data("../data/McGill-Billboard")
+notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+for note in notes:
+    print(note, "with tonic C:", key_irrespective_chord("C", Chord(note + ":maj")))
+print(key_irrespective_chord("C", Chord("Bb:min")))
+#print(key_irrespective_chord("A", Chord("G:min")))
