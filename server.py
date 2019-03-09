159c2652df65418eaebe3876d9405e30c5394507
melissachen2000 <31445526+melissachen2000@users.noreply.github.com>
2019-03-09 16:57:27 -0600

    Separates API calls into two calls

--- server.py
+++ server.py
@@ -1,24 +1,32 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_jsonpify import jsonify
+from pychord import note_to_chord
+from services.note_to_name import note_to_name
 
-from services.chord_stats import getNextChord
-from services.chord_notation_converter import convert
+#from services.chord_stats import getNextChord
+from services.dummy import dummy_func
 
 app = Flask(__name__)
 api = Api(app)
 
 class GetChordName(Resource):
     def get(self):
+        midi_chord = request.args.getlist('notes')[0].split(',')
+        pychord_chord = note_to_chord([note_to_name(int(i)) for i in midi_chord])
+        result_dict = {("chord_" + str(i)) : str(pychord_chord[i]) for i in range(len(pychord_chord))}
 
+        return jsonify(result_dict)
 
-class NextChord(Resource):
+class GetNextChord(Resource):
     def get(self):
-        midi_chord = request.args.getlist('chord')[0].split(',')
+        chords = request.args.getlist('chords')[0].split(',')
 
-        return jsonify(getNextChord(convert(midi_chord), depth=None))
+        return jsonify(dummy_func(chords, depth=None))
 
-api.add_resource(NextChord, "/nextchord")
+
+api.add_resource(GetChordName, "/chordname")
+api.add_resource(GetNextChord, "/nextchord")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ services/dummy.py
@@ -0,0 +1,2 @@
+def dummy_func(chords, depth):
+    return {chord : 1 / len(chords) for chord in chords}
